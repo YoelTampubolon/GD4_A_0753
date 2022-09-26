51f6e9e8424530f12c1f12679a7f5c90ce43c0d1
Yoel Otniel P.M.T (200710753) <yoeltampubolon457@gmail.com>
2022-09-27 01:27:32 +0700

    TGS4_A_0753_fix

--- page/listSeriesPage.php
+++ page/listSeriesPage.php
@@ -6,7 +6,7 @@ solid #D40013; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 
 0.19);" >
 
     <div class="body d-flex justify-content-between">
-        <h4>SERIES <span><a href="CreateSeriesPage.php?id=.$data['id'].'"onClick="return confirm ( \'Yakin ingin menambah series?\')"><i style="color: red" class="fa fa-thin fa-square-plus fa-2x"></i></a></span></h4> 
+        <h4>SERIES <span><a href="createSeriesPage.php?id=.$data['id'].'"onClick="return confirm ( \'Are sure want to add series?\')"><i style="color: black" class="fa fa-thin fa-square-plus fa-2x"></i></a></span></h4> 
     </div>
     <hr>
         <table class="table ">
